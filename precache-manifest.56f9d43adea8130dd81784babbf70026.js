ad5e0f52b095496ce6e080914608c1596218f548
Preshin PS <preshingithub@gmail.com>
2019-10-25 21:48:21 +0530

    Updates

---
+++ precache-manifest.56f9d43adea8130dd81784babbf70026.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3d48cc8e5698cb09be5e1187b2e431c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "602eb88fd00177ed34b5",
+    "url": "/static/css/main.7f82d784.chunk.css"
+  },
+  {
+    "revision": "771b2cc00b0a2681d87d",
+    "url": "/static/js/2.360d1b7c.chunk.js"
+  },
+  {
+    "revision": "602eb88fd00177ed34b5",
+    "url": "/static/js/main.21c10b42.chunk.js"
+  },
+  {
+    "revision": "7c86e32c67dc6de3e552",
+    "url": "/static/js/runtime-main.58c47abb.js"
+  }
+]);
